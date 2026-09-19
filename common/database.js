cca88e2b3da00f941e03e68fea6b752d4bfd9c02
mihco <michalurban177@gmail.com>
2026-09-19 19:17:57 -0400

    experimenting with sequelize

--- common/database.js
+++ common/database.js
@@ -1,6 +1,24 @@
+/*
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './storage/data.db'
 });
 module.exports = sequelize;
+*/
+const SequelConfig = require('../sequelizeconfig.js')
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(SequelConfig.DB, SequelConfig.USER, SequelConfig.PASSWORD, {
+    host: SequelConfig.HOST,
+    dialect: SequelConfig.dialect
+})
+
+const db = {};
+
+db.Sequelize = Sequelize;
+
+db.sequelize = sequelize;
+
+db.user = require('/models/')
---
+++ sequelizeconfig.js
@@ -0,0 +1,7 @@
+module.export = {
+HOST: "localhost",
+USER: "root",
+PASSWORD: "",
+DB: "sequel",
+dialect: "postgresql"
+}
